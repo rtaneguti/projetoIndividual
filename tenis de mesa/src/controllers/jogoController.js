0fb7225215ac1d712449bbbf5f7a4f1b8862cc5b
rtaneguti <rafael.taneguti@sptech.school>
2025-05-31 11:37:54 -0300

    feat: Adicionar estrutura HTML, estilização e redirecionamentos na página de início após o login

--- tenis de mesa/src/controllers/jogoController.js
+++ tenis de mesa/src/controllers/jogoController.js
@@ -1,19 +1,19 @@
-var jogoModel = require("./models/jogoModel");
+// var jogoModel = require("./models/jogoModel");
 
-function cadastrar(req, res) {
-    var nome = req.body.nome;
+// function cadastrar(req, res) {
+//     var nome = req.body.nome;
 
-    if (nome == undefined) {
-        res.status(400).send("Seu nome está undefined!");
-    }
+//     if (nome == undefined) {
+//         res.status(400).send("Seu nome está undefined!");
+//     }
 
-    jogoModel.cadastrar(nome).then(function(resposta){
-        res.status(200).send("Dados inseridos com sucesso");
-    }).catch(function(erro){
-        res.status(500).json(erro.sqlMessage);
-    })
-}
+//     jogoModel.cadastrar(nome).then(function(resposta){
+//         res.status(200).send("Dados inseridos com sucesso");
+//     }).catch(function(erro){
+//         res.status(500).json(erro.sqlMessage);
+//     })
+// }
 
-module.exports = {
-    cadastrar
-}
+// module.exports = {
+//     cadastrar
+// }
--- tenis de mesa/src/routes/rota_jogo.js
+++ tenis de mesa/src/routes/rota_jogo.js
@@ -1,11 +1,11 @@
-var express = require("express");
-var router = express.Router();
+// var express = require("express");
+// var router = express.Router();
 
-var jogoController = require("../controllers/jogoController");
+// var jogoController = require("../controllers/jogoController");
 
-router.post("/cadastrar", function (req, res) {
-    // função a ser chamada quando acessar 
-    jogoController.cadastrar(req, res);
-});
+// router.post("/cadastrar", function (req, res) {
+//     // função a ser chamada quando acessar 
+//     jogoController.cadastrar(req, res);
+// });
 
-module.exports = router;
+// module.exports = router;
